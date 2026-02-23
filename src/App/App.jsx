400e10f41aba9ecafd603c2b80226039dd93a6d7
Rares <Rares.Stefea@cognizant.com>
2026-02-23 16:28:58 +0200

    Created main landing page, left to be added are some cards with icons.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 //import style from "./App.module.css";
-
+import Landing from "../Components/Pages/Landing/Landing";
 function App() {
   return (
     <>
-      <p>hello!</p>
+      <Landing />
     </>
   );
 }
--- src/Components/Pages/Landing/Landing.jsx
+++ src/Components/Pages/Landing/Landing.jsx
@@ -1,4 +1,33 @@
 import React from "react";
-//import style from "./Landing.module.css";
+import style from "./Landing.module.css";
+import heroImg from "../../../assets/images/Hero-Landing-Page.png";
 
-export default function Landing() {}
+export default function Landing() {
+  return (
+    <div className={style.landingWrapper}>
+      <main className={style.landingRightSide}>
+        <h1 className={style.landingHeader}>Welcome Lorem ipsum!</h1>
+        <p className={style.landingParagraph}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          ultrices tellus non massa consequat porttitor. Duis eu sagittis velit,
+          id mattis risus. Donec sollicitudin, orci quis laoreet semper, purus
+          justo bibendum neque, sed volutpat erat est et felis. Donec non tempor
+          nibh. Nullam commodo augue vel nisi iaculis finibus. Integer varius
+          diam ut nulla pretium ornare. Sed mollis sapien eget augue lobortis,
+          vitae ornare nisl elementum. Nunc ut lobortis risus.
+        </p>
+        <div className={style.landingButtonWrapper}>
+          <button>Insert URL</button>
+          <button>Login/Register</button>
+        </div>
+      </main>
+      <aside className={style.landingHero}>
+        <img
+          className={style.heroImg}
+          src={heroImg}
+          alt="Image of a person checking a shopping list."
+        />
+      </aside>
+    </div>
+  );
+}
