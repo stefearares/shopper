f18023a49d72c0ffbc2eb5d8ad09e39019d6bf91
Rares <Rares.Stefea@cognizant.com>
2026-02-24 09:53:02 +0200

    Added footer.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,12 @@
 import React from "react";
-//import style from "./Footer.module.css";
+import style from "./Footer.module.css";
 
-export default function Footer() {}
+export default function Footer() {
+  return (
+    <footer className={style.footerWrapper}>
+      <div className={style.footerContent}>
+        <span> Shopper @ 2026</span>
+      </div>
+    </footer>
+  );
+}
